baeba43ae8a8dd4ae492ed52f0b958a77604a998
InsanityAI <insanityaiiii@gmail.com>
2023-07-21 11:18:07 +0200

    program metadata

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 "use strict";
 import a from "./module1/module1";
 import { program } from "commander";
+import {NAME, DESCRIPTION, VERSION} from "./metadata";
 
+program.name(NAME).version(VERSION).description(DESCRIPTION);
+program.parse();
 console.log("This is a: ", a)
