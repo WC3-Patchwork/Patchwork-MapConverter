9c783c5f3e1e3e433714a24490011a5071255e10
InsanityAI <insanityaiiii@gmail.com>
2023-07-24 23:39:11 +0200

    refactored war2json

--- src/converter/War2JsonService.ts
+++ src/converter/War2JsonService.ts
@@ -20,10 +20,10 @@ import path from 'path'
 const log = LoggerFactory.createLogger('War2Json')
 
 let translatorCount = 0
-async function processFile<T> (file: string, translator: Translator<T>, output: string): Promise<boolean> {
+async function processFile<T> (input: string, translator: Translator<T>, output: string): Promise<boolean> {
   const asyncLog = log.getSubLogger({ name: `${translator.constructor.name}-${translatorCount++}` })
-  asyncLog.info(`Starting processing ${file}`)
-  const buffer = await readFile(file)
+  asyncLog.info('Processing', input)
+  const buffer = await readFile(input)
   const result = translator.warToJson(buffer)
   if (result.errors != null && result.errors.length > 0) {
     for (const error of result.errors) {
@@ -33,7 +33,7 @@ async function processFile<T> (file: string, translator: Translator<T>, output: 
   } else {
     await mkdir(path.dirname(output), { recursive: true })
     await writeFile(output, JSON.stringify(result.json), { encoding: 'utf8' })
-    asyncLog.info(`Finished processing ${file}`)
+    asyncLog.info('Finished processing', output)
     return true
   }
 }
@@ -47,10 +47,39 @@ async function copyFileWithDirCreation (input: string, output: string): Promise<
   asyncLog.info('Copied into', output)
 }
 
-const unitsRegex = /units/i
+const translatorRecord: Record<string, Translator<unknown>> = {
+  // World files
+  '.w3e': TerrainTranslator.getInstance() as Translator<unknown>,
+  'Units.doo': UnitsTranslator.getInstance() as Translator<unknown>,
+  '.doo': DoodadsTranslator.getInstance() as Translator<unknown>,
+  '.w3r': RegionsTranslator.getInstance() as Translator<unknown>,
+  '.w3c': CamerasTranslator.getInstance() as Translator<unknown>,
+  '.w3s': SoundsTranslator.getInstance() as Translator<unknown>,
+
+  // Object data files
+  '.w3u': ObjectsTranslator.getInstance(Data.ObjectType.Units) as Translator<unknown>,
+  '.w3t': ObjectsTranslator.getInstance(Data.ObjectType.Items) as Translator<unknown>,
+  '.w3a': ObjectsTranslator.getInstance(Data.ObjectType.Abilities) as Translator<unknown>,
+  '.w3b': ObjectsTranslator.getInstance(Data.ObjectType.Destructables) as Translator<unknown>,
+  '.w3d': ObjectsTranslator.getInstance(Data.ObjectType.Doodads) as Translator<unknown>,
+  '.w3q': ObjectsTranslator.getInstance(Data.ObjectType.Upgrades) as Translator<unknown>,
+  '.w3h': ObjectsTranslator.getInstance(Data.ObjectType.Buffs) as Translator<unknown>,
+
+  // Trigger files
+  // '.lua': MISSING
+  // '.j': MISSING
+  '.wts': StringsTranslator.getInstance() as Translator<unknown>,
+
+  // Map files
+  '.w3i': InfoTranslator.getInstance() as Translator<unknown>,
+  '.imp': ImportsTranslator.getInstance() as Translator<unknown>
+  // '.wpm': MISSING
+  // '.shd': MISSING
+}
+
 const War2JsonService = {
   convert: async function (inputPath: string, outputPath: string) {
-    log.info(`Converting Warcraft III binaries in '${inputPath}' and outputting to '${outputPath}'`)
+    log.info('Converting Warcraft III binaries in', inputPath, 'and outputting to', outputPath)
 
     const promises: Array<Promise<boolean> | Promise<void>> = []
     const fileStack: Array<DirectoryTree<Record<string, unknown>>> = [directoryTree(inputPath, { attributes: ['type', 'extension'] })]
@@ -69,72 +98,12 @@ const War2JsonService = {
         }
       } else {
         let translator: Translator<unknown> | null = null
-
-        switch (file.extension) {
-          // World files
-          case '.w3e': // Terrain
-            translator = TerrainTranslator.getInstance() as Translator<unknown>
+        for (const entry of Object.entries(translatorRecord)) {
+          const searchString = entry[0]
+          if (file.name.includes(searchString)) {
+            translator = entry[1]
             break
-          case '.doo':
-            if (unitsRegex.test(file.name)) { // Preplaced units
-              translator = UnitsTranslator.getInstance() as Translator<unknown>
-            } else { // Doodads
-              translator = DoodadsTranslator.getInstance() as Translator<unknown>
-            }
-            break
-          case '.w3r': // Regions
-            translator = RegionsTranslator.getInstance() as Translator<unknown>
-            break
-          case '.w3c': // Cameras
-            translator = CamerasTranslator.getInstance() as Translator<unknown>
-            break
-          case '.w3s': // Sound definitions
-            translator = SoundsTranslator.getInstance() as Translator<unknown>
-            break
-
-            // Object data files
-          case '.w3u': // Units - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Units) as Translator<unknown>
-            break
-          case '.w3t': // Items - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Items) as Translator<unknown>
-            break
-          case '.w3a': // Abilities - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Abilities) as Translator<unknown>
-            break
-          case '.w3b': // Destructables - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Destructables) as Translator<unknown>
-            break
-          case '.w3d': // Doodads - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Doodads) as Translator<unknown>
-            break
-          case '.w3q': // Upgrades - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Upgrades) as Translator<unknown>
-            break
-          case '.w3h': // Buffs - Objects
-            translator = ObjectsTranslator.getInstance(Data.ObjectType.Buffs) as Translator<unknown>
-            break
-
-            // Trigger files
-            // case 'lua': // Lua
-            //   break
-            // case 'j': // Jass
-            //   break
-          case '.wts': // Strings
-            translator = StringsTranslator.getInstance() as Translator<unknown>
-            break
-
-            // Map files
-          case '.w3i': // Info
-            translator = InfoTranslator.getInstance() as Translator<unknown>
-            break
-          case '.imp': // Imported files
-            translator = ImportsTranslator.getInstance() as Translator<unknown>
-            break
-            // case 'wpm': // Pathing
-            //   break
-            // case 'war3map.shd': // Shadow map
-            //   break
+          }
         }
         if (translator != null) {
           const outputFile = path.join(outputPath, path.relative(inputPath, file.path)) + '.json'
