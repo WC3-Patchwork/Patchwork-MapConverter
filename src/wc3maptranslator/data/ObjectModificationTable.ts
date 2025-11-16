08a03dcd36f081f19103cc28e6f1609ed0e011b6
InsanityAI <insanityaiiii@gmail.com>
2025-11-16 14:59:05 +0100

    feat: object data multiversion conversion support

--- src/wc3maptranslator/data/ObjectModificationTable.ts
+++ src/wc3maptranslator/data/ObjectModificationTable.ts
@@ -1,23 +1,20 @@
-enum TableType {
-  original = 'original',
-  custom = 'custom'
-}
+import { type integer } from '../CommonInterfaces'
 
 enum ModificationType {
-  int = 'int',
-  real = 'real',
-  unreal = 'unreal',
-  string = 'string'
+  INTEGER = 'INTEGER', // 0
+  REAL = 'REAL', // 1
+  UNREAL = 'UNREAL', // 2 - real but with value within [0.00, 1.00]
+  STRING = 'STRING' // 3
 }
 
-enum FileTypeExtension { // (*) - uses the two optional ints after variable type
+enum FileTypeExtension {
   units = 'w3u',
   items = 'w3t',
   destructables = 'w3b',
-  doodads = 'w3d', // (*)
-  abilities = 'w3a', // (*)
+  doodads = 'w3d',
+  abilities = 'w3a',
   buffs = 'w3h',
-  upgrades = 'w3q' // (*)
+  upgrades = 'w3q'
 }
 
 enum ObjectType {
@@ -32,20 +29,19 @@ enum ObjectType {
 
 interface Modification {
   id: string
-  type: ModificationType // 'int' | 'real' | 'unreal' | 'string',
-  value: unknown
+  type: ModificationType
+  value: integer | number | string
 
   // Marked optional because these fields are not needed on any table.
   // They can be specified for: Doodads, Abilities, Upgrades, but if
   // not specified, they default to the value 0.
-  level?: number
-  column?: number
-  variation?: number
+  levelVariation?: integer
+  dataPointer?: integer
 }
 
 interface ObjectModificationTable {
   original: Record<string, Modification[]>
-  custom: Record<string, Modification[]>
+  custom?: Record<string, Modification[]>
 }
 
-export { TableType, ModificationType, FileTypeExtension, ObjectType, type Modification, type ObjectModificationTable }
+export { ModificationType, FileTypeExtension, ObjectType, type Modification, type ObjectModificationTable }
--- src/wc3maptranslator/translators/ObjectsTranslator.ts
+++ src/wc3maptranslator/translators/ObjectsTranslator.ts
@@ -1,224 +1,212 @@
+import { LoggerFactory } from '../../logging/LoggerFactory'
+import { type integer } from '../CommonInterfaces'
 import { HexBuffer } from '../HexBuffer'
 import { W3Buffer } from '../W3Buffer'
-import { type WarResult, type JsonResult } from '../CommonInterfaces'
-import { type Translator } from './Translator'
-import { type ObjectModificationTable, ObjectType, TableType, type Modification, ModificationType } from '../data/ObjectModificationTable'
+import { ModificationType, type ObjectModificationTable, ObjectType, type Modification } from '../data/ObjectModificationTable'
 
-export class ObjectsTranslator implements Translator<ObjectModificationTable> {
-  private static readonly instances = new Map<ObjectType, ObjectsTranslator>()
+const log = LoggerFactory.createLogger('ObjectsTranslator')
 
-  private readonly type: ObjectType
+enum TableType {
+  original = 'original',
+  custom = 'custom'
+}
 
-  private constructor (type: ObjectType) {
-    this.type = type
+export function jsonToWar (json: ObjectModificationTable, objectType: ObjectType, formatVersion: integer): Buffer {
+  if (formatVersion < 0 || formatVersion > 3) {
+    throw new Error(`Unknown object definition format version=${formatVersion}, expected value from range [0, 3]`)
   }
+  const output = new HexBuffer()
+  output.addInt(formatVersion)
 
-  public static getInstance (type: ObjectType): ObjectsTranslator {
-    let instance = this.instances.get(type)
-    if (instance == null) {
-      instance = new this(type)
-      this.instances.set(type, instance)
-    }
-    return instance
-  }
-
-  public static jsonToWar (type: ObjectType, info: ObjectModificationTable): WarResult {
-    return this.getInstance(type).jsonToWar(info)
-  }
-
-  public static warToJson (type: ObjectType, buffer: Buffer): JsonResult<ObjectModificationTable> {
-    return this.getInstance(type).warToJson(buffer)
-  }
-
-  // Expose the ObjectType enum as part of this abstract class
-  // The enum could be "export"ed , but it wouldn't be accessible
-  // via `ObjectsTranslator.ObjectType`, which is preferable.
-  public static readonly ObjectType = ObjectType
-
-  private static readonly varTypes = {
-    int: 0,
-    real: 1,
-    unreal: 2,
-    string: 3,
-    0: 'int',
-    1: 'real',
-    2: 'unreal',
-    3: 'string'
-  }
-
-  public jsonToWar (json: ObjectModificationTable): WarResult {
-    const outBufferToWar = new HexBuffer()
-
-    /*
-         * Header
-         */
-    outBufferToWar.addInt(2) // file version
-
-    const generateTableFromJson = (tableType: TableType, tableData: Record<string, Modification[]>): void => { // create "original" or "custom" table
-      if (!tableData) {
-        outBufferToWar.addInt(0)
-        return
+  const generateTableFromJson = (tableType: TableType, tableData: Record<string, Modification[]>): void => {
+    const data = Object.entries(tableData)
+    output.addInt(data.length)
+    data.forEach(([defKey, modifications]) => {
+      if (tableType === TableType.original) {
+        output.addChars(defKey)
+        output.addChars('\0\0\0\0')
+      } else {
+        // e.g. "h000:hfoo"
+        output.addChars(defKey.substring(5, 9)) // original id
+        output.addChars(defKey.substring(0, 4)) // custom id
       }
-      const data = Object.entries(tableData)
-      outBufferToWar.addInt(data.length)
-      data.forEach(([defKey, obj]) => {
-        // Original and new object ids
-        if (tableType === TableType.original) {
-          outBufferToWar.addChars(defKey)
-          outBufferToWar.addByte(0); outBufferToWar.addByte(0); outBufferToWar.addByte(0); outBufferToWar.addByte(0) // no new Id is assigned
-        } else {
-          // e.g. "h000:hfoo"
-          outBufferToWar.addChars(defKey.substring(5, 9)) // original id
-          outBufferToWar.addChars(defKey.substring(0, 4)) // custom id
+
+      if (formatVersion >= 3) {
+        // once Blizzard decides to do something with this, I will add Set/Variant support
+        // TODO: sets means adding another for loop, just take a look at warToJson function
+        output.addInt(1) // setCount
+        output.addInt(0) // setFlag
+      }
+
+      output.addInt(modifications.length)
+      for (const modification of modifications) {
+        output.addChars(modification.id)
+
+        const fieldTypeValue = (function (fieldType: ModificationType): integer {
+          switch (fieldType) {
+            case ModificationType.INTEGER:
+              return 0
+            case ModificationType.REAL:
+              return 1
+            case ModificationType.UNREAL:
+              return 2
+            case ModificationType.STRING:
+              return 3
+          }
+        })(modification.type)
+        output.addInt(fieldTypeValue)
+
+        let useExtraData: boolean
+        switch (objectType) {
+          case ObjectType.Abilities:
+          case ObjectType.Upgrades:
+            useExtraData = true
+            break
+          case ObjectType.Doodads:
+            useExtraData = formatVersion > 0x01
+            break
+          default:
+            useExtraData = false
+            break
         }
 
-        // Number of modifications made to this object
-        outBufferToWar.addInt(obj?.length || 0)
+        if (useExtraData) {
+          output.addInt(modification.levelVariation ?? 0) // Level or variation
+          output.addInt(modification.dataPointer ?? 0) // E.g DataA1 is 1 because of col A; refer to the xyzData.slk files for Data fields
+        }
 
-        obj?.forEach((mod: Modification) => {
-          let modType: number
+        switch (modification.type) {
+          case ModificationType.INTEGER:
+            output.addInt(modification.value as integer)
+            break
+          case ModificationType.REAL:
+          case ModificationType.UNREAL:
+            output.addFloat(modification.value as number)
+            break
+          case ModificationType.STRING:
+            output.addString(modification.value as string)
+            break
+        }
 
-          // Modification id (e.g. unam = name; reference MetaData lookups)
-          outBufferToWar.addChars(mod.id)
+        if (formatVersion > 0x00) {
+          output.addChars(defKey)
+        }
+      }
+    })
+  }
 
-          // Determine what type of field the mod is (int, real, unreal, string)
-          if (mod.type != null) { // if a type is specified, use it
-            modType = ObjectsTranslator.varTypes[mod.type]
-          } else { // otherwise we try to infer between int/string (note there is no way to detect unreal or float this way, so user must specify those explicitly)
-            if (typeof mod.value === 'number') {
-              modType = ObjectsTranslator.varTypes.int
-            } else if (typeof mod.value === 'string') {
-              modType = ObjectsTranslator.varTypes.string
-            } else {
-              throw new Error('No type specified and cannot infer type!')
-            }
-          }
-
-          outBufferToWar.addInt(modType)
-
-          // Addl integers
-          // Required for: doodads, abilities, upgrades
-          if (this.type === ObjectType.Doodads || this.type === ObjectType.Abilities || this.type === ObjectType.Upgrades) {
-            // Level or variation
-            // We need to check if hasOwnProperty because these could be explititly
-            // set to 0, but JavaScript's truthiness evaluates to false to it was defaulting
-            outBufferToWar.addInt((mod.level ?? mod.variation) ?? 0) // defaults to 0
-
-            outBufferToWar.addInt(mod.column ?? 0) // E.g DataA1 is 1 because of col A; refer to the xyzData.slk files for Data fields
-          }
-
-          // Write mod value
-          if (modType === ObjectsTranslator.varTypes.int) {
-            outBufferToWar.addInt(mod.value as number)
-          } else if (modType === ObjectsTranslator.varTypes.real || modType === ObjectsTranslator.varTypes.unreal) {
-            // Follow-up: check if unreal values are same hex format as real
-            outBufferToWar.addFloat(mod.value as number)
-          } else if (modType === ObjectsTranslator.varTypes.string) {
-            // Note that World Editor normally creates a TRIGSTR_000 for these string
-            // values - WC3MapTranslator just writes the string directly to file
-            outBufferToWar.addString(mod.value as string)
-          }
-
-          // End of struct
-          if (tableType === TableType.original) {
-            // Original objects are ended with their base id (e.g. hfoo)
-            outBufferToWar.addChars(defKey)
-          } else {
-            // Custom objects are ended with 0000 bytes
-            outBufferToWar.addByte(0)
-            outBufferToWar.addByte(0)
-            outBufferToWar.addByte(0)
-            outBufferToWar.addByte(0)
-          }
-        })
-      })
-    }
-
-    generateTableFromJson(TableType.original, json.original)
+  generateTableFromJson(TableType.original, json.original)
+  if (json.custom) {
     generateTableFromJson(TableType.custom, json.custom)
-
-    return {
-      errors: [],
-      buffer: outBufferToWar.getBuffer()
-    }
   }
 
-  public warToJson (buffer: Buffer): JsonResult<ObjectModificationTable> {
-    const result: ObjectModificationTable = { original: {}, custom: {} }
-    const outBufferToJSON = new W3Buffer(buffer)
+  return output.getBuffer()
+}
 
-    const fileVersion = outBufferToJSON.readInt()
+export function warToJson (buffer: Buffer, objectType: ObjectType): ObjectModificationTable {
+  const input = new W3Buffer(buffer)
+  const formatVersion = input.readInt()
+  if (formatVersion < 0 || formatVersion > 3) {
+    log.warn(`Unknown object definition format version ${formatVersion} will attempt at reading...`)
+  }
 
-    const readModificationTable = (isOriginalTable: boolean): void => {
-      const numTableModifications = outBufferToJSON.readInt()
+  const readModificationTable = (input: W3Buffer, objectType: ObjectType, formatVersion: integer): Record<string, Modification[]> => {
+    const modifications: Record<string, Modification[]> = {}
 
-      for (let i = 0; i < numTableModifications; i++) {
-        const objectDefinition: Modification[] = [] // object definition will store one or more modification objects
+    const numTableModifications = input.readInt()
+    for (let i = 0; i < numTableModifications; i++) {
+      const objectDefinition: Modification[] = []
 
-        const originalId = outBufferToJSON.readChars(4)
-        const customId = outBufferToJSON.readChars(4)
-        let sets: number
-        if (fileVersion >= 3) {
-          sets = outBufferToJSON.readInt()
-        } else {
-          sets = 1
+      const originalId = input.readChars(4)
+      const customId = input.readChars(4)
+      let sets: number
+      if (formatVersion >= 3) {
+        sets = input.readInt()
+      } else {
+        sets = 1
+      }
+
+      for (let j = 0; j < sets; j++) {
+        if (formatVersion >= 3) {
+          input.readInt() // setFlag unused in editor, who knows. (Variant in WE)
         }
+        const modificationCount = input.readInt()
+        for (let k = 0; k < modificationCount; k++) {
+          const fieldId = input.readChars(4)
+          const fieldTypeValue = input.readInt()
 
-        for (let j = 0; j < sets; j++) {
-          if (fileVersion >= 3) {
-            const setFlag = outBufferToJSON.readInt()
+          let useExtraData: boolean
+          switch (objectType) {
+            case ObjectType.Abilities:
+            case ObjectType.Upgrades:
+              useExtraData = true
+              break
+            case ObjectType.Doodads:
+              useExtraData = formatVersion > 0x01
+              break
+            default:
+              useExtraData = false
+              break
           }
-          const modificationCount = outBufferToJSON.readInt()
 
-          for (let k = 0; k < modificationCount; k++) {
-            const modification: Modification = {
-              id: '',
-              type: ModificationType.string,
-              level: 0,
-              column: 0,
-              value: {}
-            }
-
-            modification.id = outBufferToJSON.readChars(4)
-            modification.type = ObjectsTranslator.varTypes[outBufferToJSON.readInt()] as unknown as ModificationType // 'int' | 'real' | 'unreal' | 'string',
-
-            if (this.type === ObjectType.Doodads || this.type === ObjectType.Abilities || this.type === ObjectType.Upgrades) {
-              modification.level = outBufferToJSON.readInt()
-              modification.column = outBufferToJSON.readInt()
-            }
-
-            if (modification.type === 'int') {
-              modification.value = outBufferToJSON.readInt()
-            } else if (modification.type === 'real' || modification.type === 'unreal') {
-              modification.value = outBufferToJSON.readFloat()
-            } else { // modification.type === 'string'
-              modification.value = outBufferToJSON.readString()
-            }
-
-            if (isOriginalTable) {
-              outBufferToJSON.readInt() // should be 0 for original objects
-            } else {
-              outBufferToJSON.readChars(4) // should be object ID for custom objects
-            }
-
-            objectDefinition.push(modification)
+          let levelVariant: integer
+          let dataPointer: integer
+          if (useExtraData) {
+            levelVariant = input.readInt()
+            dataPointer = input.readInt()
+          } else {
+            levelVariant = -1
+            dataPointer = -1
           }
-        }
 
-        if (isOriginalTable) {
-          result.original[originalId] = objectDefinition
-        } else {
-          result.custom[`${customId}:${originalId}`] = objectDefinition
+          let fieldType: ModificationType
+          let fieldValue: integer | number | string
+          switch (fieldTypeValue) {
+            case 0:
+              fieldType = ModificationType.INTEGER
+              fieldValue = input.readInt()
+              break
+            case 1:
+              fieldType = ModificationType.REAL
+              fieldValue = input.readFloat()
+              break
+            case 2:
+              fieldType = ModificationType.UNREAL
+              fieldValue = input.readFloat()
+              break
+            case 3:
+              fieldType = ModificationType.STRING
+              fieldValue = input.readString()
+              break
+            default:
+              throw new Error(`Unknown object modification type ${fieldTypeValue} for '${originalId}'->'${fieldId}'`)
+          }
+
+          if (formatVersion > 0x00) {
+            input.readChars(4) // objectID, again
+          }
+
+          objectDefinition.push({
+            id: fieldId,
+            type: fieldType,
+            value: fieldValue,
+            levelVariation: levelVariant,
+            dataPointer
+          })
         }
       }
-    }
 
-    readModificationTable(true)
-    readModificationTable(false)
-
-    return {
-      errors: [],
-      json: result
+      const objectKey = (customId === '\0\0\0\0') ? originalId : `${customId}:${originalId}`
+      modifications[objectKey] = objectDefinition
     }
+    return modifications
   }
+
+  const original = readModificationTable(input, objectType, formatVersion)
+  let custom: Record<string, Modification[]> | undefined
+  if (!input.isExhausted()) {
+    custom = readModificationTable(input, objectType, formatVersion)
+  }
+
+  return { original, custom }
 }
