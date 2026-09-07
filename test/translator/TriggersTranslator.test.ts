809ad8b54ee60b6cb2770cb7e6b59ed42e4cc654
InsanityAI <insanityaiiii@gmail.com>
2026-09-07 20:53:13 +0000

    feat: trigger translator test

--- test/translator/TriggersTranslator.test.ts
+++ test/translator/TriggersTranslator.test.ts
@@ -1,10 +1,13 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { CustomScript, GUITrigger, MapHeader, translators, TriggerComment, TriggerContainer } from '../../src/translator'
+import { CustomScript, GUITrigger, MapHeader, Statement, translators, TriggerComment, TriggerContainer } from '../../src/translator'
 import { StatementType } from '../../src/translator/data/statement/StatementType'
+import { ParameterType } from '../../src/translator/data/parameter/ParameterType'
 import { TriggerTranslatorOutput } from '../../src/translator/TriggersTranslator';
 import { ContentType } from '../../src/translator/data/content/TriggerContent';
+import { TriggerDataRegistry } from '../../src/enhancements/TriggerDataRegistry';
 const TriggersTranslator = translators.TriggersTranslator
+TriggerDataRegistry.loadTriggerData('test/triggerdata.txt')
 
 const header = {
     contentType: ContentType.HEADER,
@@ -37,33 +40,188 @@ const trigger = {
     description: 'Lel',
     events: [{
         type: StatementType.EVENT,
-        name: '',
+        name: 'TriggerRegisterPlayerUnitEventSimple',
         isEnabled: true,
         parameters: [
             {
-
+                type: ParameterType.PRESET,
+                value: 'Player00'
+            },
+            {
+                type: ParameterType.PRESET,
+                value: 'PlayerUnitEventDeath'
             }
         ]
     }],
     conditions: [{
         type: StatementType.CONDITION,
-        name: '',
+        name: 'OperatorCompareBoolean',
         isEnabled: true,
         parameters: [
             {
-
+                type: ParameterType.FUNCTION,
+                value: 'IsUnitInGroup',
+                statement: {
+                    type: StatementType.CALL,
+                    name: 'IsUnitInGroup',
+                    isEnabled: true,
+                    parameters: [
+                        {
+                            type: ParameterType.FUNCTION,
+                            value: 'GetTriggerUnit',
+                            statement: {
+                                type: StatementType.CALL,
+                                name: 'GetTriggerUnit',
+                                isEnabled: true
+                            }
+                        },
+                        {
+                            type: ParameterType.FUNCTION,
+                            value: 'GetUnitsInRangeOfLocAll',
+                            statement: {
+                                type: StatementType.CALL,
+                                name: 'GetUnitsInRangeOfLocAll',
+                                isEnabled: true,
+                                parameters: [
+                                    {
+                                        type: ParameterType.VALUE,
+                                        value: '128.00'
+                                    },
+                                    {
+                                        type: ParameterType.FUNCTION,
+                                        value: 'Location',
+                                        statement: {
+                                            type: StatementType.CALL,
+                                            name: 'Location',
+                                            isEnabled: true,
+                                            parameters: [
+                                                {
+                                                    type: ParameterType.VALUE,
+                                                    value: '0.00'
+                                                },
+                                                {
+                                                    type: ParameterType.VALUE,
+                                                    value: '0.00'
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                } 
+            },
+            {
+                type: ParameterType.PRESET,
+                value: 'EqualNotEqualOperator'
+            },
+            {
+                type: ParameterType.VALUE,
+                value: 'true'
             }
         ]
     }],
     actions: [{
         type: StatementType.ACTION,
-        name: '',
+        name: 'ForGroupMultiple',
         isEnabled: true,
         parameters: [
             {
-
+                type: ParameterType.FUNCTION,
+                value: 'GetUnitsInRangeOfLocAll',
+                statement: {
+                    type: StatementType.CALL,
+                    name: 'GetUnitsInRangeOfLocAll',
+                    isEnabled: true,
+                    parameters: [
+                        {
+                            type: ParameterType.VALUE,
+                            value: '128.00'
+                        },
+                        {
+                            type: ParameterType.FUNCTION,
+                            value: 'Location',
+                            statement: {
+                                type: StatementType.CALL,
+                                name: 'Location',
+                                isEnabled: true,
+                                parameters: [
+                                    {
+                                        type: ParameterType.VALUE,
+                                        value: '0.00'
+                                    },
+                                    {
+                                        type: ParameterType.VALUE,
+                                        value: '0.00'
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
             }
-        ]
+        ],
+        statements: {
+            [0]: [{
+                type: StatementType.ACTION,
+                name: 'KillUnit',
+                isEnabled: true,
+                parameters: [
+                    {
+                        type: ParameterType.FUNCTION,
+                        value: 'GetEnumUnit',
+                        statement: {
+                            type: StatementType.CALL,
+                            name: 'GetEnumUnit',
+                            isEnabled: true
+                        }
+                    }
+                ]
+            },
+            {
+                type: StatementType.ACTION,
+                name: 'SetVariable',
+                isEnabled: true,
+                parameters: [
+                    {
+                        type: ParameterType.VARIABLE,
+                        value: 'TempInteger',
+                        arrayIndex: {
+                            type: ParameterType.VALUE,
+                            value: '1'
+                        }
+                    },
+                    {
+                        type: ParameterType.FUNCTION,
+                        value: 'OperatorInt',
+                        statement: {
+                            type: StatementType.CALL,
+                            name: 'OperatorInt',
+                            isEnabled: true,
+                            parameters: [
+                                {
+                                    type: ParameterType.VARIABLE,
+                                    value: 'TempInteger',
+                                    arrayIndex: {
+                                        type: ParameterType.VALUE,
+                                        value: '1'
+                                    }
+                                },
+                                {
+                                    type: ParameterType.PRESET,
+                                    value: 'OperatorAdd'
+                                },
+                                {
+                                    type: ParameterType.VALUE,
+                                    value: '1'
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }] as Statement[]
+        } as Record<number, Statement[]>
     }]
 } as GUITrigger
 
@@ -86,7 +244,7 @@ const testData: TriggerTranslatorOutput = {
 const output = TriggersTranslator.jsonToWar(testData, 7, 2, 2147483652)
 const [readData, formatVersion, variableFormatVersion, formatSubversion] = TriggersTranslator.warToJson(output)
 
-describe('Sound data translation', () => {
+describe('Triggers translation', () => {
     it('Trigger format versions match', () => {
         expect(formatVersion).to.equal(7)
     })
